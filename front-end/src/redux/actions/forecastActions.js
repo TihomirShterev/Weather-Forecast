eb145cd7ad221c76e8b462ddc9532cf98ed85826
Tihomir Shterev <tihomir.shterev@app-streams.com>
2021-06-14 16:30:42 +0300

    Added request to server error handling

--- front-end/src/redux/actions/forecastActions.js
+++ front-end/src/redux/actions/forecastActions.js
@@ -1,32 +1,53 @@
 import { mapsURL, mapsKey, baseURL, apiKey, historyURL } from "../../config/config";
-import { ActionTypes } from "../constants/actionTypes";
+import ActionTypes from "../constants/actionTypes";
 import previousFiveDays from "../../utils/constants";
 
 export const fetchCoordinates = (cityName, isoCode) => async dispatch => {
-  const res = await fetch(`${mapsURL}?address=${cityName}+${isoCode}&key=${mapsKey}`);
-  const data = await res.json();
-  const lat = Number(data.results[0].geometry.location.lat.toFixed(4));
-  const lon = Number(data.results[0].geometry.location.lng.toFixed(4));
-  dispatch({
-    type: ActionTypes.FETCH_COORDINATES,
-    payload: { lat, lon }
-  });
+  try {
+    const res = await fetch(`${mapsURL}?address=${cityName}+${isoCode}&key=${mapsKey}`);
+    const data = await res.json();
+    const lat = Number(data.results[0].geometry.location.lat.toFixed(4));
+    const lon = Number(data.results[0].geometry.location.lng.toFixed(4));
+    dispatch({
+      type: ActionTypes.FETCH_COORDINATES,
+      payload: { lat, lon }
+    });
+  } catch (err) {
+    dispatch({
+      type: ActionTypes.SERVER_ERROR,
+      payload: { errorMessage: err }
+    });
+  }
 };
 
 export const fetchFullWeatherInfo = (lat, lon) => async dispatch => {
-  const res = await fetch(`${baseURL}?lat=${lat}&lon=${lon}&appid=${apiKey}`);
-  const data = await res.json();
-  dispatch({
-    type: ActionTypes.FETCH_FULL_WEATHER_INFO,
-    payload: data
-  });
+  try {
+    const res = await fetch(`${baseURL}?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+    const data = await res.json();
+    dispatch({
+      type: ActionTypes.FETCH_FULL_WEATHER_INFO,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: ActionTypes.SERVER_ERROR,
+      payload: { errorMessage: err }
+    });
+  }
 };
 
 export const fetchPreviousDayInfo = (lat, lon, i) => async (dispatch) => {
-  const res = await fetch(`${historyURL}?lat=${lat}&lon=${lon}&dt=${previousFiveDays[i % 6]}&appid=${apiKey}`);
-  const data = await res.json();
-  dispatch({
-    type: ActionTypes.FETCH_PREVIOUS_DAY_INFO,
-    payload: data
-  });
+  try {
+    const res = await fetch(`${historyURL}?lat=${lat}&lon=${lon}&dt=${previousFiveDays[i % 6]}&appid=${apiKey}`);
+    const data = await res.json();
+    dispatch({
+      type: ActionTypes.FETCH_PREVIOUS_DAY_INFO,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: ActionTypes.SERVER_ERROR,
+      payload: { errorMessage: err }
+    });
+  }
 };
