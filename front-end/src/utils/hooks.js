1466ee3378c273b072dd67b566b589ceb98abcf0
Tihomir Shterev <tihomir.shterev.1@gmail.com>
2022-05-06 11:59:16 +0300

    Optimized useForecastData hook

--- front-end/src/utils/hooks.js
+++ front-end/src/utils/hooks.js
@@ -4,10 +4,5 @@ import { cities } from "./constants";
 export const useForecastData = () => {
   const { city } = useParams();
   const current = cities.find(({ val }) => city === val);
-
-  if (current) {
-    return { lat: current.lat, lon: current.lon };
-  } else {
-    return <Redirect to="/" />;
-  }
+  return current || <Redirect to="/" />;
 };
