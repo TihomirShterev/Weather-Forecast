d2a6ce268149d5273dd44694219ef375ae8e96f3
Tihomir Shterev <tihomir.shterev.1@gmail.com>
2022-05-06 12:00:33 +0300

    Fixed header unknown location crash

--- front-end/src/components/common/header/index.js
+++ front-end/src/components/common/header/index.js
@@ -1,12 +1,11 @@
 import React, { useLayoutEffect, useState } from 'react';
-import { Link, useLocation, useParams } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useForecastData } from '../../../utils/hooks';
 import styles from './index.module.css';
-import { cities } from '../../../utils/constants';
 
 const Header = () => {
-  const { city } = useParams();
-  const { name, country, val } = cities.find(({ val }) => city === val);
   const location = useLocation();
+  const { val, name, country } = useForecastData();
   const [currentClass, setCurrentClass] = useState('');
   const [dailyClass, setDailyClass] = useState('');
 
@@ -18,27 +17,28 @@ const Header = () => {
     }
   }, [location.pathname, val]);
 
-
   return (
     <header className={styles.header}>
       <div className={styles.location}>
         <h1>{name}</h1>
         <h2>{country}</h2>
       </div>
-      <ul>
-        <li className={styles[`${currentClass}`]}>
-          <Link to={`/${val}/current`}>В момента</Link>
-        </li>
-        <li className={styles[`${dailyClass}`]}>
-          <Link to={`/${val}/daily`}>24 часа</Link>
-        </li>
-        <li>
-          <Link to={`/${val}/ten-day`}>10 дни</Link>
-        </li>
-        <li>
-          <Link to={`/${val}/weekend`}>Уикенд</Link>
-        </li>
-      </ul>
+      {val && (
+        <ul>
+          <li className={styles[`${currentClass}`]}>
+            <Link to={`/${val}/current`}>В момента</Link>
+          </li>
+          <li className={styles[`${dailyClass}`]}>
+            <Link to={`/${val}/daily`}>24 часа</Link>
+          </li>
+          <li>
+            <Link to={`/${val}/ten-day`}>10 дни</Link>
+          </li>
+          <li>
+            <Link to={`/${val}/weekend`}>Уикенд</Link>
+          </li>
+        </ul>
+      )}
     </header>
   );
 };
