f72a2935c6633af944b3aa3ed178cb176f5b17ea
Tihomir Shterev <tihomir.shterev.1@gmail.com>
2022-05-06 11:22:20 +0300

    Removed coordinates fetching
    Disabled Google Geocoding API.

--- front-end/src/utils/hooks.js
+++ front-end/src/utils/hooks.js
@@ -1,24 +1,13 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Redirect, useParams } from "react-router";
 import { cities } from "./constants";
-import { fetchCoordinates } from "../redux/actions/forecastActions";
-
-const selectWeatherInfo = state => state.forecast;
 
 export const useForecastData = () => {
   const { city } = useParams();
   const current = cities.find(({ val }) => city === val);
-  const dispatch = useDispatch();
-  const { lat, lon } = useSelector(selectWeatherInfo);
 
-  useEffect(() => {
-    dispatch(fetchCoordinates(current.val, current.isoCode));
-  }, [current.isoCode, current.val, dispatch]);
-
-  if (!current) {
+  if (current) {
+    return { lat: current.lat, lon: current.lon };
+  } else {
     return <Redirect to="/" />;
   }
-
-  return { lat, lon };
 };
